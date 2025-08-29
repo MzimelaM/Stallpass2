c192d2dcba9afd24510ac62fbedb8ce134b46a3f
Simangaliso <230070728@mycput.ac.za>
2025-08-29 12:30:16 +0200

    App state

--- lib/state/app_state.dart
+++ lib/state/app_state.dart
@@ -1,18 +1,40 @@
-// lib/state/app_state.dart
 import 'package:flutter/foundation.dart';
+//import '../models.dart';
 
 class AppState extends ChangeNotifier {
-  int? _userId;
+  String? _studentNumber; // use String now
+  int _score = 0;
+  int _streak = 0;
+  final Set<String> _completedStallIds = {};
 
-  int? get userId => _userId;
+  String? get studentNumber => _studentNumber;
+  int get score => _score;
+  int get streak => _streak;
+  Set<String> get completedStallIds => _completedStallIds;
 
-  void setUserId(int? userId) {
-    _userId = userId;
+  void setStudentNumber(String newStudentNumber) {
+    _studentNumber = newStudentNumber;
     notifyListeners();
   }
 
-  void clearUser() {
-    _userId = null;
+  void addScore(int delta) {
+    _score += delta;
+    notifyListeners();
+  }
+
+  //void markStallCompleted(Stall stall) {
+   // if (_completedStallIds.add(stall.id)) {
+   //   _streak = _completedStallIds.length;
+   //   notifyListeners();
+   // }
+ // }
+
+  bool isStallCompleted(String stallId) => _completedStallIds.contains(stallId);
+
+  void resetProgress() {
+    _score = 0;
+    _streak = 0;
+    _completedStallIds.clear();
     notifyListeners();
   }
 }
